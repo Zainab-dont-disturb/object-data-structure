da73cf37f2f6a6eacab12d80d3b660faed837814
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-10-21 16:20:03 +0100

    done

---
+++ scrpt.js
@@ -0,0 +1,10 @@
+var student = {
+name:"Zainab",
+age:13,
+gender:"female",
+}
+var color{"blue", "pink", "purple"}
+    function idk(){
+    document.getElementById("change").innerHTML=("my name is "+ student["name"] + "i am " + student["age"]);
+    }
+    document.write("my name is "+ student["name"] + "i am " + student["age"]);
